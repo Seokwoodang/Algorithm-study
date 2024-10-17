5c73dec0164971a863f8714d14ed9e615920aa80
Seokwoodang <swoo1429@naver.com>
2024-10-17 15:03:11 +0900

    add : 봉우리 ( 5 / 10 )

---
+++ section2/mountainTop.js
@@ -0,0 +1,41 @@
+const solution = (arr) => {
+  let count = 0;
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+  let n = arr.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      let flag = 1;
+      for (let k = 0; k < 4; k++) {
+        let nx = i + dx[k];
+        let ny = j + dy[k];
+        console.log(arr[i][j]);
+        if (
+          nx >= 0 &&
+          nx < n &&
+          ny >= 0 &&
+          ny < n &&
+          arr[nx][ny] >= arr[i][j]
+        ) {
+          console.log(arr[nx][ny], arr[i][j]);
+          flag = 0;
+          break;
+        }
+      }
+      if (flag) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(
+  solution([
+    [5, 3, 7, 2, 3],
+    [3, 7, 1, 6, 1],
+    [7, 2, 5, 3, 4],
+    [4, 3, 6, 4, 1],
+    [8, 7, 3, 5, 2],
+  ])
+);
