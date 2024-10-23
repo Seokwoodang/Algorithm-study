86ba9cfc686b5f8a4912419a8b8e27da80d35a34
Seokwoodang <swoo1429@naver.com>
2024-10-23 21:03:24 +0900

    add : 멘토 ( 6 / 10 )

---
+++ section4/mentor.js
@@ -0,0 +1,26 @@
+const solution = (arr) => {
+  let answer;
+  let count={};
+  for(let i = 0; i<arr.length;i++){
+    for(let j = 0; j<arr[i][j];j++){
+        if(count[i])
+    } 
+  }
+  return answer;
+};
+
+console.log(
+  solution([
+    [3, 4, 1, 2],
+    [4, 3, 2, 1],
+    [3, 1, 4, 2],
+  ])
+);
+
+
+
+<div>
+  <div>
+    <button></button>
+  </div>
+</div>
