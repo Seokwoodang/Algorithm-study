1ed41f74bf17a2d098ee5fe84dfd3f64a8430d89
Seokwoodang <swoo1429@naver.com>
2024-10-24 13:30:34 +0900

    add : 멘토 ( 7 / 10 )

--- section4/mentor.js
+++ section4/mentor.js
@@ -1,10 +1,27 @@
 const solution = (arr) => {
-  let answer;
-  let count={};
-  for(let i = 0; i<arr.length;i++){
-    for(let j = 0; j<arr[i][j];j++){
-        if(count[i])
-    } 
+  let answer = 0;
+  let m = arr.length;
+  let n = arr[0].length;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      let count = 0;
+      for (let k = 0; k < m; k++) {
+        let pi = 0;
+        let pj = 0;
+        for (let s = 0; s < n; s++) {
+          if (arr[k][s] === i) {
+            pi = s;
+          }
+          if (arr[k][s] === j) {
+            pj = s;
+          }
+        }
+        if (pi < pj) {
+          count++;
+        }
+      }
+      if (count === m) answer++;
+    }
   }
   return answer;
 };
@@ -16,11 +33,3 @@ console.log(
     [3, 1, 4, 2],
   ])
 );
-
-
-
-<div>
-  <div>
-    <button></button>
-  </div>
-</div>
