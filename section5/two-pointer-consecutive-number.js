c76e9028b0103967e489da28c1ef8a2dcb670185
Seokwoodang <swoo1429@naver.com>
2024-10-25 14:10:33 +0900

    add : 투 포인터 - 연속부분수열 ( 5 / 10 )

---
+++ section5/two-pointer-consecutive-number.js
@@ -0,0 +1,17 @@
+const solution = (num, arr) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let suming = i;
+    let sum = 0;
+    while (sum <= num) {
+      sum += arr[suming];
+      suming++;
+      if (sum === num) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+console.log(solution(6, [1, 2, 1, 3, 1, 1, 1, 2]));
