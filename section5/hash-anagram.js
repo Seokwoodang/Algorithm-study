b5b584de56584d8546412d9ed552a3a8adea22cc
Seokwoodang <swoo1429@naver.com>
2024-10-28 22:12:43 +0900

    add : 해쉬 맵 - 아나그램 ( 5 / 10 )

---
+++ section5/hash-anagram.js
@@ -0,0 +1,15 @@
+const solution = (str1, str2) => {
+  let sH = new Map();
+  for (const x of str1) {
+    if (sH.has(x)) sH.set(x, sH.get(x) + 1);
+    else sH.set(x, 1);
+  }
+  for (const x of str2) {
+    if (!sH.has(x) || sH.get(x) === 0) return "No";
+    sH.set(x, sH.get(x) - 1);
+  }
+  return "Yes";
+};
+
+console.log(solution("AbaAeCe", "baeeACA"));
+console.log(solution("abaCC", "Caaab"));
