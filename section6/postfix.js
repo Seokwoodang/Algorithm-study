462c7cfe11aa4b2d21162155ef24018f24709aa5
Seokwoodang <swoo1429@naver.com>
2024-10-31 16:52:20 +0900

    add : 후위식(postfix) 연산(스택) ( 5 / 10 )

---
+++ section6/postfix.js
@@ -0,0 +1,18 @@
+const solution = (str) => {
+  let stack = [];
+  for (const x of str) {
+    if (!isNaN(x)) {
+      stack.push(Number(x));
+    } else {
+      let rt = stack.pop();
+      let lt = stack.pop();
+      if (x === "+") stack.push(lt + rt);
+      else if (x === "-") stack.push(lt - rt);
+      else if (x === "*") stack.push(lt * rt);
+      else if (x === "/") stack.push(lt / rt);
+    }
+  }
+  return stack[0];
+};
+
+console.log(solution("35-"));
