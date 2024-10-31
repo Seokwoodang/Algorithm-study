bf60e77358e53ad042d47fc7005dd143e454a40f
Seokwoodang <swoo1429@naver.com>
2024-10-31 17:36:49 +0900

    add : 쇠막대기(스택) ( 5 / 10 )

---
+++ section6/stack-ironRod.js
@@ -0,0 +1,21 @@
+// 입력이 괄호가 들어온다면 10중 9는 stack
+
+const solution = (str) => {
+  let answer = 0;
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") stack.push(str[i]);
+    else if (str[i] === ")" && str[i - 1] === "(") {
+      stack.pop();
+      answer += stack.length;
+    } else {
+      stack.pop();
+      answer++;
+    }
+  }
+  return answer;
+};
+
+console.log(solution("()(((()())(())()))(())"));
+console.log(solution("(((()(()()))(())()))(()())"));
