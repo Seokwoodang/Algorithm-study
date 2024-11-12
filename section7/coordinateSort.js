982ee53f77ce133f420c4f15c1927b9c2aa39e59
Seokwoodang <swoo1429@naver.com>
2024-11-12 13:40:51 +0900

    add : 좌표정렬 ( 3 / 10 )

---
+++ section7/coordinateSort.js
@@ -0,0 +1,22 @@
+//title:좌표정렬
+//level:3
+//etc:sort()_한층깊이_사용하기
+
+const solution = (arr) => {
+  let answer = arr;
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    else return a[0] - b[0];
+  });
+  return answer;
+};
+
+console.log(
+  solution([
+    [2, 7],
+    [1, 3],
+    [1, 2],
+    [2, 5],
+    [3, 6],
+  ])
+);
