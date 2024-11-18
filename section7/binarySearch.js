ef8fd92a5e926c9ccb063609cecd54945ed7de5b
Seokwoodang <swoo1429@naver.com>
2024-11-18 13:47:23 +0900

    fix : 이분검색 ( 5 / 10 )

--- section7/binarySearch.js
+++ section7/binarySearch.js
@@ -1,6 +1,6 @@
 //title:이분검색
 //level:5
-//etc:[이분검색블로그정리](https://inblog.ai/luke/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B0%9C%EB%85%90-%EC%A0%95%EB%A6%AC-33865#%EC%9D%B4%EB%B6%84%EA%B2%80%EC%83%89-13)
+//etc:[이분검색_블로그정리](https://inblog.ai/luke/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B0%9C%EB%85%90-%EC%A0%95%EB%A6%AC-33865#%EC%9D%B4%EB%B6%84%EA%B2%80%EC%83%89-13)
 
 const solution = (target, arr) => {
   let answer;
