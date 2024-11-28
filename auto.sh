ac07538f49113aba96ba9536a553e3d69507204d
Seokwoodang <swoo1429@naver.com>
2024-11-29 00:19:53 +0900

    test : auto.sh 3

--- auto.sh
+++ auto.sh
@@ -1,4 +1,3 @@
-
 JS_DIRS=$(find "." -type f -name "*.js" | sort -u)
 JS_CNT=$(find "." -type f -name "*.js" | sort -u | wc -l)
 
@@ -15,29 +14,29 @@ echo "| --- | --- | -- | -- | --- |" >> README.md
 entries=()
 
 for JS_DIR in $JS_DIRS; do
-
     FULL_DATETIME=$(git log --diff-filter=A --format='%ad' --date=iso -- $JS_DIR)
-    DATETIME=$(git log --diff-filter=A --format=%ad --date=short -- $JS_DIR)
-    title=$(cat $JS_DIR | grep "//title:" | sed -n 's/.*\/\/title:\(.*\)/\1/p')
-    level=$(cat $JS_DIR | grep "//level:" | sed -n 's/.*\/\/level:\(.*\)/\1/p')
+    DATETIME=$(git log --diff-filter=A --format='%ad' --date=short -- $JS_DIR)
+    title=$(grep "//title:" "$JS_DIR" | sed -n 's/.*\/\/title:\(.*\)/\1/p')
+    level=$(grep "//level:" "$JS_DIR" | sed -n 's/.*\/\/level:\(.*\)/\1/p')
     etc=$(grep "//etc:" "$JS_DIR" | sed -n 's/.*\/\/etc:\(.*\)/\1/p')
-    JS_FILE=$(basename $JS_DIR)
-    
+    JS_FILE=$(basename "$JS_DIR")
     entries+=("$(echo "$FULL_DATETIME|$DATETIME|$title|$level|[$JS_FILE]($JS_DIR)|$etc" | tr -d '\r')")
-
 done
 
-for entry in $(printf "%s\n" "${entries[@]}" | sort); do
+for entry in $(printf "%s\n" "${entries[@]}" | sort -t "|" -k 1); do
     IFS="|" read -r full_datetime date title level file etc <<< "$entry"
     echo "| $title | $level | $file | $date | $etc |" >> README.md
-done 
+done
 
-git add .
-IS_GENERATED_MD=$(git status | grep -e README.md)
-if [ -z "$IS_GENERATED_MD" ]; then
-    echo "hi"
-else
+# Remove any blank lines
+sed -i '/^$/d' README.md
+
+git add README.md
+IS_GENERATED_MD=$(git status --porcelain | grep README.md)
+
+if [ -n "$IS_GENERATED_MD" ]; then
     git commit -m "add : README.md" --author "Seokwoodang <swoo1429@naver.com>"
-    git remote -v
     git push origin
+else
+    echo "No changes detected in README.md"
 fi
